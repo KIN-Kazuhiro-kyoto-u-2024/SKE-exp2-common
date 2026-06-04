291b5f6d0f0d10736309f311b2a45c2e8a2f2489
KIN <jin.yihao.88p@st.kyoto-u.ac.jp>
2026-06-04 16:06:03 +0900

    blackとisortでフォーマットした

--- rl/env.py
+++ rl/env.py
@@ -1,33 +1,47 @@
-import numpy as np
-from simulator.acrobot import swingup, balance, sys_iden
 from collections import OrderedDict
 
+import numpy as np
+
+from simulator.acrobot import balance, swingup, sys_iden
+
+
 def make_env(config):
-    if config.domain == 'double_pendulum':
-        if config.task == 'swingup':
+    if config.domain == "double_pendulum":
+        if config.task == "swingup":
             return SwingUp(config)
-        elif config.task == 'balance':
+        elif config.task == "balance":
             return Balance(config)
-        elif config.task == 'sys_iden':
+        elif config.task == "sys_iden":
             return SysIden(config)
     else:
         raise NotImplementedError
 
-class Balance():
+
+class Balance:
 
     def __init__(self, config):
-        
+
         self._config = config
         d = config.num_digitized
         self._env = balance()
-        self._pendulum_limit = (-0.20*np.pi, 0.20*np.pi)     # alpha の範囲．これを超えたら倒立維持失敗．
-        self._arm_limit = (-0.9*np.pi, 0.9*np.pi)            # theta の範囲．これを超えたら倒立維持失敗．
-        
-        print('dt = ', config.repeat*self._env.control_timestep())
-        self._digitized_action = np.linspace(self._env.action_spec().minimum[0], self._env.action_spec().maximum[0], config.num_action)
-        print('action interval: ', self._digitized_action)
-        print('state-action is ', f"({d}x{d}x{d}x{d}) x {config.num_action}")
-        print(f'total state-action number is {(d**2)*((d)**2)*config.num_action}')
+        self._pendulum_limit = (
+            -0.20 * np.pi,
+            0.20 * np.pi,
+        )  # alpha の範囲．これを超えたら倒立維持失敗．
+        self._arm_limit = (
+            -0.9 * np.pi,
+            0.9 * np.pi,
+        )  # theta の範囲．これを超えたら倒立維持失敗．
+
+        print("dt = ", config.repeat * self._env.control_timestep())
+        self._digitized_action = np.linspace(
+            self._env.action_spec().minimum[0],
+            self._env.action_spec().maximum[0],
+            config.num_action,
+        )
+        print("action interval: ", self._digitized_action)
+        print("state-action is ", f"({d}x{d}x{d}x{d}) x {config.num_action}")
+        print(f"total state-action number is {(d**2)*((d)**2)*config.num_action}")
 
     @property
     def action_space(self):
@@ -36,10 +50,10 @@ class Balance():
     @property
     def obs_space(self):
         return self._config.state_size
-    
+
     def step(self, action, time=None, timedepedence=None):
         action = self._digitized_action[action]
-        for _ in range(self._config.repeat):   
+        for _ in range(self._config.repeat):
             obs = self._env.step(action).observation
         digitized_state, done, state_dict = self._digitized_state(obs)
         reward, best = self._get_reward(state_dict, done, action)
@@ -56,11 +70,14 @@ class Balance():
         # 倒立状態の維持に失敗したときの報酬
         if done:
             return -10, 0
-        
+
         # 報酬（rew）の設定
         # rew の与え方を色々変更してみる
         d = self._config.num_digitized
-        n_pendulum_rad, n_pendulum_vel = state_dict["n_pendulum_rad"], state_dict["n_pendulum_vel"]
+        n_pendulum_rad, n_pendulum_vel = (
+            state_dict["n_pendulum_rad"],
+            state_dict["n_pendulum_vel"],
+        )
         n_best = (d - 1) / 2
         n_arm_rad, n_arm_vel = state_dict["n_arm_rad"], state_dict["n_arm_vel"]
         n_arm_best = (d - 1) / 2
@@ -81,10 +98,17 @@ class Balance():
 
         # arm_rad: theta, arm_vel: thetadot, pendulum_rad: alpha, pendulum_vel: alphadot
         # 以下を書き換えて，これら（連続値）の離散化方法を色々変更してみる
-        n_arm_rad = np.digitize(arm_rad, np.linspace(self._arm_limit[0], self._arm_limit[1], d+1)[1:-1])
-        n_arm_vel = np.digitize(arm_vel.clip(-8, 8), np.linspace(-8, 8, d+1)[1:-1])
-        n_pendulum_rad = np.digitize(pendulum_rad, np.linspace(self._pendulum_limit[0], self._pendulum_limit[1], d+1)[1:-1])
-        n_pendulum_vel = np.digitize(pendulum_vel.clip(-8, 8), np.linspace(-8, 8, d+1)[1:-1])
+        n_arm_rad = np.digitize(
+            arm_rad, np.linspace(self._arm_limit[0], self._arm_limit[1], d + 1)[1:-1]
+        )
+        n_arm_vel = np.digitize(arm_vel.clip(-8, 8), np.linspace(-8, 8, d + 1)[1:-1])
+        n_pendulum_rad = np.digitize(
+            pendulum_rad,
+            np.linspace(self._pendulum_limit[0], self._pendulum_limit[1], d + 1)[1:-1],
+        )
+        n_pendulum_vel = np.digitize(
+            pendulum_vel.clip(-8, 8), np.linspace(-8, 8, d + 1)[1:-1]
+        )
 
         state_dict["arm_rad"] = arm_rad
         state_dict["pendulum_rad"] = pendulum_rad
@@ -94,27 +118,31 @@ class Balance():
         state_dict["n_pendulum_rad"] = n_pendulum_rad
         state_dict["n_arm_vel"] = n_arm_vel
         state_dict["n_pendulum_vel"] = n_pendulum_vel
-        state_dict["digitized_state"] = n_pendulum_rad + n_pendulum_vel*d + n_arm_vel*d**2 + n_arm_rad*d**3
+        state_dict["digitized_state"] = (
+            n_pendulum_rad + n_pendulum_vel * d + n_arm_vel * d**2 + n_arm_rad * d**3
+        )
 
         # theta か alpha が既定の範囲を超えたら（倒立状態の維持に失敗したら）
         # done を True としてエピソード終了
         arm_cond = n_arm_rad == 0 or n_arm_rad == d - 1
-        pendulum_cond = n_pendulum_rad == 0 or n_pendulum_rad == d -1
+        pendulum_cond = n_pendulum_rad == 0 or n_pendulum_rad == d - 1
         if arm_cond or pendulum_cond:
             done = True
         else:
             done = False
         return state_dict["digitized_state"], done, state_dict
-    
+
+
 ####################################
 ## SysIden: システム同定用の処理 #####
 ####################################
 
-class SysIden():
+
+class SysIden:
     def __init__(self, config):
         self._config = config
         self._env = sys_iden(init_alpha=config.init_alpha)
-        print('dt = ', config.repeat * self._env.control_timestep())
+        print("dt = ", config.repeat * self._env.control_timestep())
 
     @property
     def action_space(self):
@@ -123,9 +151,9 @@ class SysIden():
     @property
     def obs_space(self):
         return self._env.observation_spec()
-    
+
     def step(self, action, time=None, timedepedence=None):
-        for _ in range(self._config.repeat):   
+        for _ in range(self._config.repeat):
             obs = self._env.step(action).observation
         state_dict, done = self._get_obs(obs)
         reward = 0
@@ -137,7 +165,7 @@ class SysIden():
         return state_dict
 
     def _get_obs(self, obs):
-        # 0 is positive z axis at doule pendulum pendulum angle 
+        # 0 is positive z axis at doule pendulum pendulum angle
         state_dict = OrderedDict()
         vec, vel = obs["orientations"], obs["velocity"]
         arm_vec, pendulum_vec = vec[0:2], vec[2:4]
@@ -151,36 +179,50 @@ class SysIden():
         state_dict["alpha_dot"] = pendulum_vel
         done = False
         return state_dict, done
-    
+
+
 #####################################
 ## 以下は共通課題では不要 #############
 #####################################
 
-class SwingUp():
+
+class SwingUp:
     def __init__(self, config):
         self._config = config
-        self._arrange = np.zeros(config.num_digitized-1)
+        self._arrange = np.zeros(config.num_digitized - 1)
         self._env = swingup()
-        print('dt = ', self._env.control_timestep())
+        print("dt = ", self._env.control_timestep())
         # self._arrange[config.num_digitized//2] = -0.20
         # self._arrange[config.num_digitized//2 - 2] = 0.20
-        self._digitized_action = np.linspace(self._env.action_spec().minimum[0], self._env.action_spec().maximum[0], config.num_action)
-        print('alpha interval: ', np.linspace(-np.pi, np.pi, config.num_digitized+1)[1:-1] + self._arrange)
-        print('theta interval: ', np.linspace(-np.pi, np.pi, config.num_digitized +1)[1:-1])
+        self._digitized_action = np.linspace(
+            self._env.action_spec().minimum[0],
+            self._env.action_spec().maximum[0],
+            config.num_action,
+        )
+        print(
+            "alpha interval: ",
+            np.linspace(-np.pi, np.pi, config.num_digitized + 1)[1:-1] + self._arrange,
+        )
+        print(
+            "theta interval: ",
+            np.linspace(-np.pi, np.pi, config.num_digitized + 1)[1:-1],
+        )
         d = config.num_digitized
-        print('action interval: ', self._digitized_action)
-        print('state-action is ', f"({d}x{d}x{d}x{d}) x {config.num_action}")
-        print(f'total state-action number is {(d**2)*((d)**2)*config.num_action}')
+        print("action interval: ", self._digitized_action)
+        print("state-action is ", f"({d}x{d}x{d}x{d}) x {config.num_action}")
+        print(f"total state-action number is {(d**2)*((d)**2)*config.num_action}")
+
     @property
     def action_space(self):
         return self._config.num_action
+
     @property
     def obs_space(self):
         return self._config.state_size
-    
+
     def step(self, action, time=None, timedepedence=None):
         action = self._digitized_action[action]
-        for _ in range(self._config.repeat):   
+        for _ in range(self._config.repeat):
             obs = self._env.step(action).observation
         digitized_state, done, state_dict = self._digitized_state(obs)
         reward, best = self._get_reward(state_dict, done)
@@ -196,7 +238,7 @@ class SwingUp():
         pass
 
     def _digitized_state(self, obs):
-        # 0 is positive z axis at doule pendulum pendulum angle 
+        # 0 is positive z axis at doule pendulum pendulum angle
         state_dict = OrderedDict()
         d = self._config.num_digitized
         vec, vel = obs["orientations"], obs["velocity"]
@@ -204,11 +246,15 @@ class SwingUp():
         arm_vel, pendulum_vel = vel[0], vel[1]
         arm_rad = np.arctan2(arm_vec[1], arm_vec[0])
         pendulum_rad = np.arctan2(pendulum_vec[1], pendulum_vec[0])
-        n_arm_rad = np.digitize(arm_rad, np.linspace(-np.pi, np.pi, d +1)[1:-1])
-        n_arm_vel = np.digitize(arm_vel.clip(-8, 8), np.linspace(-8, 8, d+1)[1:-1])
+        n_arm_rad = np.digitize(arm_rad, np.linspace(-np.pi, np.pi, d + 1)[1:-1])
+        n_arm_vel = np.digitize(arm_vel.clip(-8, 8), np.linspace(-8, 8, d + 1)[1:-1])
 
-        n_pendulum_rad = np.digitize(pendulum_rad, np.linspace(-np.pi, np.pi, d+1)[1:-1] + self._arrange)
-        n_pendulum_vel = np.digitize(pendulum_vel.clip(-8, 8), np.linspace(-8, 8, d+1)[1:-1])
+        n_pendulum_rad = np.digitize(
+            pendulum_rad, np.linspace(-np.pi, np.pi, d + 1)[1:-1] + self._arrange
+        )
+        n_pendulum_vel = np.digitize(
+            pendulum_vel.clip(-8, 8), np.linspace(-8, 8, d + 1)[1:-1]
+        )
 
         state_dict["arm_rad"] = arm_rad
         state_dict["pendulum_rad"] = pendulum_rad
@@ -218,16 +264,20 @@ class SwingUp():
         state_dict["n_pendulum_rad"] = n_pendulum_rad
         state_dict["n_arm_vel"] = n_arm_vel
         state_dict["n_pendulum_vel"] = n_pendulum_vel
-        state_dict["digitized_state"] = n_pendulum_rad + n_pendulum_vel*d + n_arm_vel*d**2 + n_arm_rad*d**3
+        state_dict["digitized_state"] = (
+            n_pendulum_rad + n_pendulum_vel * d + n_arm_vel * d**2 + n_arm_rad * d**3
+        )
         if n_arm_rad == 0 or n_arm_rad == d - 1:
             done = True
         else:
             done = False
         return state_dict["digitized_state"], done, state_dict
 
+
 def main():
+    import time
     from dataclasses import dataclass
-    import time 
+
     from PIL import Image
 
     @dataclass
@@ -241,21 +291,25 @@ def main():
         alpha: float = 0.5
         max_episode: int = int(10e3)
         episode_length: int = 400
-        should_log_model: int = (10e3)
+        should_log_model: int = 10e3
         should_log_scalar: int = int(10)
         should_log_video: int = int(50)
         restore: bool = False
         restore_file: str = "Qtable.npy"
         video_length: int = 400
         logdir: str = "./logs/" + str(time.strftime("%Y-%m-%d-%H-%M-%S")) + "/"
+
     env = SwingUp(EnvConfig())
     # env = suite.load(domain_name="acrobot", task_name="swingup")
     env.reset()
     for i in range(100):
-        img = Image.fromarray(env._env.physics.render(height=480, width=640,camera_id=0))
+        img = Image.fromarray(
+            env._env.physics.render(height=480, width=640, camera_id=0)
+        )
         img.save("./img.png")
         for i in range(10):
             env.step(2)
+
 
 if __name__ == "__main__":
     main()
--- rl/train.py
+++ rl/train.py
@@ -1,18 +1,21 @@
-import numpy as np
-from dataclasses import dataclass
 import os
 import pathlib
+import shutil
 import sys
 import time
-import shutil
+from dataclasses import dataclass
+
+import numpy as np
 
 directory = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(directory))
 
+from collections import OrderedDict
+
 from env import make_env
 from models import Qtable
+
 from utils.logger import Logger
-from collections import OrderedDict
 
 
 @dataclass
@@ -20,23 +23,25 @@ class EnvConfig:
     domain: str = "double_pendulum"
     task: str = "balance"
     repeat: int = 2
-    num_digitized: int = 8         # 振れ角 theta および alpha に対する離散値の数
-    num_action: int = 4            # アクション（入力トルク）に対する離散値の数
+    num_digitized: int = 8  # 振れ角 theta および alpha に対する離散値の数
+    num_action: int = 4  # アクション（入力トルク）に対する離散値の数
     state_size: int = num_digitized**4
-    gamma: float = 0.99            # 割引率
-    alpha: float = 0.5             # 学習率
-    max_episode: int = int(10e4)   # 学習の総 episode 数
-    episode_length: int = 200      # 1 episode のタイムステップ数
-    should_log_model: int = 1000   # 何 episode おきに QTable の値を保存するか
-    should_log_scalar: int = 200   # 何 episode おきに学習のログを出力するか
-    should_log_video: int = 1000   # 何 episode おきに QTable の評価をするか
-    restore: bool = False          # 学習を再スタートする場合，これを True とする
-    restore_path: str = ""         # QTable の初期値用データのパスを入力（再スタート時）
-    video_length: int = 200        # QTable 評価時におけるタイムステップ数
-    logdir: str = pathlib.Path().joinpath("./logs/train", str(time.strftime("%m-%d-%H-%M-%S")))
+    gamma: float = 0.99  # 割引率
+    alpha: float = 0.5  # 学習率
+    max_episode: int = int(10e4)  # 学習の総 episode 数
+    episode_length: int = 200  # 1 episode のタイムステップ数
+    should_log_model: int = 1000  # 何 episode おきに QTable の値を保存するか
+    should_log_scalar: int = 200  # 何 episode おきに学習のログを出力するか
+    should_log_video: int = 1000  # 何 episode おきに QTable の評価をするか
+    restore: bool = False  # 学習を再スタートする場合，これを True とする
+    restore_path: str = ""  # QTable の初期値用データのパスを入力（再スタート時）
+    video_length: int = 200  # QTable 評価時におけるタイムステップ数
+    logdir: str = pathlib.Path().joinpath(
+        "./logs/train", str(time.strftime("%m-%d-%H-%M-%S"))
+    )
 
 
-class Agent():
+class Agent:
     def __init__(self, config: EnvConfig) -> None:
         self._config = config
         self._build_model()
@@ -49,13 +54,14 @@ class Agent():
 
     def _build_model(self):
         self._qtable = Qtable(self._config)
-    
+
+
 def main():
 
     config = EnvConfig()
     env = make_env(config)
     os.makedirs(config.logdir, exist_ok=True)
-    print(f'log: {config.logdir}')
+    print(f"log: {config.logdir}")
     logger = Logger(config.logdir)
     agent = Agent(config)
 
@@ -82,22 +88,22 @@ def main():
     total_time = 0
 
     # プログラムコードを log フォルダにコピー（後で見返す用）
-    codes_dir = str(config.logdir) + '/codes'
+    codes_dir = str(config.logdir) + "/codes"
     os.mkdir(codes_dir)
-    shutil.copy('train.py', codes_dir)
-    shutil.copy('env.py', codes_dir)
-    shutil.copy('eval.py', codes_dir)
-    shutil.copy('models.py', codes_dir)
-    shutil.copy('../simulator/acrobot.py', codes_dir)
-    shutil.copy('../simulator/acrobot.xml', codes_dir)
+    shutil.copy("train.py", codes_dir)
+    shutil.copy("env.py", codes_dir)
+    shutil.copy("eval.py", codes_dir)
+    shutil.copy("models.py", codes_dir)
+    shutil.copy("../simulator/acrobot.py", codes_dir)
+    shutil.copy("../simulator/acrobot.xml", codes_dir)
 
     # 学習データ保存用の csv ファイルを作成
-    data_file = str(config.logdir) + '/data.csv'
-    with open(data_file,'w') as df:
-        print('episode', 'ave_ep_len', 'ave_ep_rew', 'qtable_err', sep=',', file=df)
+    data_file = str(config.logdir) + "/data.csv"
+    with open(data_file, "w") as df:
+        print("episode", "ave_ep_len", "ave_ep_rew", "qtable_err", sep=",", file=df)
 
     # main training loop
-    for episode in range(config.max_episode+1):
+    for episode in range(config.max_episode + 1):
 
         # １エピソードの実行
         start_time = time.time()
@@ -105,7 +111,9 @@ def main():
         episode_reward = 0
         best_num = 0
         for step in range(config.episode_length):
-            action = agent.get_action(state, logger.global_step, method='epsilon-greedy')
+            action = agent.get_action(
+                state, logger.global_step, method="epsilon-greedy"
+            )
             next_state, reward, done, state_dict = env.step(action)
             qtable = agent.update_Qtable(state, action, reward, next_state)
             episode_reward += reward
@@ -118,21 +126,37 @@ def main():
 
         # スカラーをログに保存
         if episode % config.should_log_scalar == 0:
-            print(f"episode: {episode}, episode_reward: {episode_reward}, episode_steps: {step+1}")
+            print(
+                f"episode: {episode}, episode_reward: {episode_reward}, episode_steps: {step+1}"
+            )
             qtable_err = np.mean(np.abs((agent._qtable._Qtable - prev_q)))
             prev_q = agent._qtable._Qtable.copy()
-            logger.add_scalars(OrderedDict([
-                ("train/ep_reward", episode_reward),
-                ("train/ep_length", step),
-                ("train/ep_best_num", best_num),
-                (f"train/ave_ep_len_{config.should_log_scalar}", sum(ave_ep_len)/len(ave_ep_len)),
-                ("train/qtable_error", qtable_err),
-                ("time/total_minute", total_time / 60)
-            ]))
-            
+            logger.add_scalars(
+                OrderedDict(
+                    [
+                        ("train/ep_reward", episode_reward),
+                        ("train/ep_length", step),
+                        ("train/ep_best_num", best_num),
+                        (
+                            f"train/ave_ep_len_{config.should_log_scalar}",
+                            sum(ave_ep_len) / len(ave_ep_len),
+                        ),
+                        ("train/qtable_error", qtable_err),
+                        ("time/total_minute", total_time / 60),
+                    ]
+                )
+            )
+
             # csv ファイルにも保存
-            with open(data_file, 'a') as df:
-                print(episode, sum(ave_ep_len)/len(ave_ep_len), sum(ave_ep_rew)/len(ave_ep_rew), qtable_err, sep=',', file=df)
+            with open(data_file, "a") as df:
+                print(
+                    episode,
+                    sum(ave_ep_len) / len(ave_ep_len),
+                    sum(ave_ep_rew) / len(ave_ep_rew),
+                    qtable_err,
+                    sep=",",
+                    file=df,
+                )
 
             ave_ep_len = []
             ave_ep_rew = []
@@ -141,7 +165,7 @@ def main():
         if episode % config.should_log_model == 0 and episode != 0:
             save_file = config.logdir.joinpath(f"qtable_{episode}.npy")
             np.save(save_file, qtable)
-            print(f'\nsave model {save_file}\n')
+            print(f"\nsave model {save_file}\n")
 
         # 評価と動画の保存
         if episode % config.should_log_video == 0 and episode != 0:
@@ -155,36 +179,44 @@ def main():
             alpha_seq = []
             theta_seq = []
             for _ in range(config.video_length):
-                img_seq.append(env._env.physics.render(height=480, width=640, camera_id=0))
-                action = (agent.get_action(state, explore=False))
+                img_seq.append(
+                    env._env.physics.render(height=480, width=640, camera_id=0)
+                )
+                action = agent.get_action(state, explore=False)
                 next_state, reward, done, state_dict = env.step(action)
                 state = next_state
                 act_seq.append(env._digitized_action[action])
                 rew_seq.append(reward)
-                alpha_seq.append(state_dict['pendulum_rad'])
-                theta_seq.append(state_dict['arm_rad'])
+                alpha_seq.append(state_dict["pendulum_rad"])
+                theta_seq.append(state_dict["arm_rad"])
                 eval_reward += reward
                 best_num += state_dict["best"]
                 if done:
                     break
-            print(f"\nevaluate episode reward: {eval_reward}, episode step: {len(rew_seq)}\n")
-            logger.log_video({'eval/video': np.array(img_seq)}, save=False)
-            logger.add_scalars(OrderedDict([
-                ("eval/ep_reward", eval_reward),
-                ("eval/ep_best_num", best_num)
-                ]))
-            act_seq_img = logger.plot2image('action', {'torque': np.array(act_seq)})
-            rew_seq_img = logger.plot2image('reward', {'reward': np.array(rew_seq)})
-            state_seq_img = logger.plot2image('state', {'alpha': np.array(alpha_seq),
-                                                    'theta': np.array(theta_seq)})
-            img_dict = {'eval/action': act_seq_img,
-                        'eval/reward': rew_seq_img,
-                        'eval/state': state_seq_img}
+            print(
+                f"\nevaluate episode reward: {eval_reward}, episode step: {len(rew_seq)}\n"
+            )
+            logger.log_video({"eval/video": np.array(img_seq)}, save=False)
+            logger.add_scalars(
+                OrderedDict(
+                    [("eval/ep_reward", eval_reward), ("eval/ep_best_num", best_num)]
+                )
+            )
+            act_seq_img = logger.plot2image("action", {"torque": np.array(act_seq)})
+            rew_seq_img = logger.plot2image("reward", {"reward": np.array(rew_seq)})
+            state_seq_img = logger.plot2image(
+                "state", {"alpha": np.array(alpha_seq), "theta": np.array(theta_seq)}
+            )
+            img_dict = {
+                "eval/action": act_seq_img,
+                "eval/reward": rew_seq_img,
+                "eval/state": state_seq_img,
+            }
             logger.log_image(img_dict)
         total_time += time.time() - start_time
         start_time = time.time
         logger.step()
 
+
 if __name__ == "__main__":
     main()
-
